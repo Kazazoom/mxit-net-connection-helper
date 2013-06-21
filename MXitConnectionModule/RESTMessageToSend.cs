83f08a8cfe6223b1e115ee9c151fa3cfbce6c283
George Ziady <george@springfisher.com>
2013-06-21 13:14:32 +0800

    Added app link functionality to REST Messages.

--- MXitConnectionModule/RESTMessageToSend.cs
+++ MXitConnectionModule/RESTMessageToSend.cs
@@ -14,6 +14,7 @@ using MXit.Messaging;
 //using RestSharp;
 using Newtonsoft;
 using Newtonsoft.Json;
+using System.Text.RegularExpressions;
 //using RestSharp.Authenticators;
 
 
@@ -26,8 +27,11 @@ namespace MXitConnectionModule
 
         public String Body;
         public bool ContainsMarkup = true;
-        public String From = ""; 
+        public String From = "";
+        public List<RESTMessageLink> Links = new List<RESTMessageLink>();
         public String To = "";
+        public bool Spool = true;
+        public int SpoolTimeOut = 604800; //604800 = 7 days in seconds
         private int _ToCount = 0;
 
         public RESTMessageToSend()
@@ -44,21 +48,76 @@ namespace MXitConnectionModule
         public RESTMessageToSend(MessageToSend messageToSend)
         {
             From = messageToSend.From;
-            this.addMxitUserIDTo(messageToSend.To);
-            Body = messageToSend.Body;
+            this.addMxitUserIDTo(messageToSend.To);          
+            Body = ProcessAppLinks(messageToSend.Body, messageToSend.From);            
         }
 
         public RESTMessageToSend(String rFrom, String rBody)
         {
             From = rFrom;
-            Body = rBody;
+            Body = ProcessAppLinks(rBody, rFrom);
         }
 
         public RESTMessageToSend(String rFrom, String rTo, String rBody)
         {
             From = rFrom;
             this.addMxitUserIDTo(rTo);
-            Body = rBody;
+            Body = ProcessAppLinks(rBody, rFrom);
+        }
+
+        /// <summary>
+        /// Process any App links that may be in the message Body text
+        /// </summary>        
+        /// 
+        public string ProcessAppLinks(String msgBody, String msgFrom = "") {            
+            string regexLink = @"\[.*?\|.*?\]"; //Links should follow the format: [Try this App|appid]
+
+            MatchCollection matches = Regex.Matches(msgBody, regexLink);
+            int counter = 0;
+            if (matches.Count > 0)
+            {
+                try
+                {
+                    foreach (Match m in matches)
+                    {
+                        //break up the link [text|applink] into it's parts
+                        string matchText = m.ToString();
+                        matchText = matchText.Replace("[", "");
+                        matchText = matchText.Replace("]", "");
+
+                        String[] linkParts = matchText.Split(new char[] { '|' }, 2);
+
+                        string linkText = linkParts[0];
+                        string linkTarget = linkParts[1];
+                        
+                        //check that the links have all the necessary parts
+                        if ((linkText.Length > 0) && (linkTarget.Length > 0))
+                        {
+                            bool linkTempContact = true;
+                            //if the sender of the message matches the target link, don't open link as a new contact
+                            if (msgFrom == linkTarget)
+                            {
+                                linkTempContact = false;
+                            }
+
+                            RESTMessageLink appLink = new RESTMessageLink(linkText, linkTarget, linkTempContact);
+                            Links.Add(appLink);
+
+                            //replace the link with the placeholder text: {n}
+                            msgBody = msgBody.Replace(m.ToString(), "{" + counter.ToString() + "}");
+
+                            counter++;
+                        }
+                    }
+
+                }
+                catch (Exception e)
+                {
+                    logger.Error("Error parsing message body: " + msgBody, e);
+                }
+            }
+
+            return msgBody;
         }
 
         /// <summary>
